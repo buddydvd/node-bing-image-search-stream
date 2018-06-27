40c610a22e2db23b9ba9da2816953442d7359b4d
David Hu <davidjhu@gmail.com>
2018-06-27 12:20:24 -0700

    Terminate iterator if stream finishes before iterator finishes vending

--- src/index.js
+++ src/index.js
@@ -10,13 +10,28 @@ export default class BingImageSearchStream extends Readable {
     } else if (this.iterator.next !== 'function') {
       throw new Error('iterator is missing next();');
     }
+    this.done = false;
   }
   async _read() {
     try {
       const result = await this.iterator.next();
-      this.push(!result.done ? result.value : null);
+      this.done = result.done;
+      this.push(!this.done ? result.value : null);
     } catch (err) {
       process.nextTick(() => { this.emit('error', err); });
     }
+  }
+  async _destroy(err, cb) {
+    let error = err;
+    if (!this.done) {
+      if (typeof this.iterator.return === 'function') {
+        try {
+          await this.iterator.return();
+        } catch (err2) {
+          error = err2;
+        }
+      }
+    }
+    cb(error);
   }
 }
